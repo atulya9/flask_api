6a3ada8d8a86c85c1604fa5b4b0b993d6cb0d38c
atulya9 <tibrewalatulya@gmail.com>
2022-08-09 22:04:02 +0530

    Cleaned the code a little

--- cache_app.py
+++ cache_app.py
@@ -7,6 +7,7 @@ in_memory_cache = {}
 
 @app.get('/get/<key>')
 def get_key_value(key):
+    
     if key in in_memory_cache:
         return in_memory_cache[key]
     else:
@@ -14,36 +15,46 @@ def get_key_value(key):
 
 @app.route('/set', methods=['POST'])
 def set_key_value():
+
     new_item = request.get_json(force=True)
     for item in new_item.keys():
         in_memory_cache[item] = new_item[item]
+
     return jsonify(new_item)
 
 @app.route('/search', methods=['GET'])
 def search_prefix_suffix():
+
     prefix = request.args.get('prefix') or ''
     suffix = request.args.get('suffix') or ''
     len_prefix = len(prefix)
     len_suffix = len(suffix)
     values = []
+
     if prefix != '' and suffix != '':
+
         for item in in_memory_cache.keys():
             if len(item) >= len_prefix + len_suffix:
                 if item[:len_prefix] == prefix and item[len(item)-len_suffix:] == suffix:
                     values.append(item)
         return values if values != [] else 'Invalid prefix/suffix'
+
     elif prefix == '' and suffix != '':
+
         for item in in_memory_cache.keys():
             if len(item) >= len_suffix:
                 if item[len(item)-len_suffix:] == suffix:
                     values.append(item)
         return values if values != [] else 'Invalid prefix/suffix'
+
     elif prefix != '' and suffix == '':
+
         for item in in_memory_cache.keys():
             if len(item) >= len_prefix:
                 if item[:len_prefix] == prefix:
                     values.append(item)
         return values if values != [] else 'Invalid prefix/suffix'
+
     return 'Invalid prefix/suffix'
 
 if __name__ == "__main__":
